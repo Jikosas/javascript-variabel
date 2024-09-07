4666a20d0a1e7284ca5fb216a92534de15f135b7
Victor Sanjaya <166146650+Jikosas@users.noreply.github.com>
2024-09-07 20:02:05 +0700

    Add files via upload

---
+++ script ep4.js
@@ -0,0 +1,46 @@
+// jadi ini adalah komen, jadi bebas aja
+
+// 1.variabel dengan let
+let nama = "victor sanjaya";
+
+// tampilkan data
+console.log(nama);
+
+// kita ubah variabel nama
+nama = "kentang"
+console.log(nama);
+
+// 2. variabel dengan var
+var NamaDepan = "victor";
+console.log(NamaDepan);
+Namadepan = "good";
+console.log (NamaDepan);
+
+// Kelakuan dari var
+let namaBelakang = "Embut"
+{
+    let namaBelakang = "Ambut";
+console.log(namaBelakang);
+}
+console.log(namaBelakang);
+
+// kelakuan dari var
+var namaTengah = "keren";
+{
+    var namaTengah = "Ganteng";
+    console.log(namaTengah);
+}
+console.log(namaTengah);
+
+//kasus khusus
+gorengan = "pentol";
+{
+    gorengan ="lolot";
+}
+console.log(gorengan)
+
+// 3. const
+
+const TTL = "10 maret 2022";
+console.log(TTL);
+//TTL = "11 maret 2024"; gak boleh dilakukan karena sudah janji konstan
